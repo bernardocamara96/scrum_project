e8fddc3f59774d2f9eee21df31c74622f5a898ba
Bernardocamara96 <camarabernardo96@gmail.com>
2024-01-23 15:18:59 +0000

    login form done

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn_login = document.querySelector("#btn_login");
+
+btn_login.addEventListener("click", function () {
+   const username = document.querySelector(".login_container #username");
+   var username_txt = username.value;
+});
