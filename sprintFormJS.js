7fd5545429026098ed4f8dbb8956b9694d7c5046
Daniel Alves da Silva <danielfilipsilva@gmail.com>
2024-02-01 10:33:11 +0000

    push

--- sprintFormJS.js
+++ sprintFormJS.js
@@ -1,10 +1,11 @@
-document.querySelector("#scrum_color").style.backgroundColor = localStorage.getItem("background_color");
+document.querySelector("#scrum_color").style.backgroundColor =
+  localStorage.getItem("background_color");
 
 //Botão que guarda novo comentário
-let bGuardarComent = document.getElementById('guardarNovoComent');
+let bGuardarComent = document.getElementById("guardarNovoComent");
 
 //Botão que guarda o edit do comentário
-let bEditComment = document.getElementById('saveEdit');
+let bEditComment = document.getElementById("saveEdit");
 
 //Obter o Modal
 let modal = document.getElementById("myModal");
@@ -17,113 +18,119 @@ let span = document.getElementsByClassName("close")[0];
 
 //Ao clicar no botão, abrir Modal
 bOpenModal.onclick = function () {
+  //Botão de guardar fica visível
+  bGuardarComent.style.display = "block";
 
-   //Botão de guardar fica visível
-   bGuardarComent.style.display = "block";
+  document.getElementById("areaComentario").value = ""; //Limpa textarea
+  document.getElementById("categoriaComentario").value = "none"; //Limpa select
+  document.getElementById("selectMember").value = "none"; //Limpa select
 
-   document.getElementById('areaComentario').value = ""; //Limpa textarea
-   document.getElementById('categoriaComentario').value = "none"; //Limpa select
-   document.getElementById('selectMember').value = "none"; //Limpa select
+  modal.style.display = "block";
 
-   modal.style.display = "block";
-
-   console.log(listComments);
+  console.log(listComments);
 };
 
 //Ao clicar no (x), fechar Modal
 span.onclick = function () {
-   console.log("Botão x clicado");
-   modal.style.display = "none";
-   //Reset display dos botões para ficarem invisiveis
-   bGuardarComent.style.display = "none";
-   bEditComment.style.display = "none";
+  console.log("Botão x clicado");
+  modal.style.display = "none";
+  //Reset display dos botões para ficarem invisiveis
+  bGuardarComent.style.display = "none";
+  bEditComment.style.display = "none";
 };
 
 let listComments = new Array();
 
 //Ao clicar no botão, escreve informação na tabela
 bGuardarComent.onclick = function () {
-   console.log("click guardar");
+  console.log("click guardar");
 
-   if (categoriaComentario.value != "none" && selectMember.value != "none" && areaComentario.value != "") {
-      //Esconde Modal
-      modal.style.display = "none";
+  if (
+    categoriaComentario.value != "none" &&
+    selectMember.value != "none" &&
+    areaComentario.value != ""
+  ) {
+    //Esconde Modal
+    modal.style.display = "none";
 
-      const comentario_cell = document.createElement("td");
-      const categ_cell = document.createElement("td");
-      const member_cell = document.createElement("td");
-      const btn_edit_cell = document.createElement("td");
-      const btn_edit = document.createElement("button");
-      const novaLinhaTab = document.createElement("tr");
+    const comentario_cell = document.createElement("td");
+    const categ_cell = document.createElement("td");
+    const member_cell = document.createElement("td");
+    const btn_edit_cell = document.createElement("td");
+    const btn_edit = document.createElement("button");
+    const novaLinhaTab = document.createElement("tr");
 
-      //Add novo comentário ao array
-      const newComment = [areaComentario.value, categoriaComentario.value, selectMember.value];
+    //Add novo comentário ao array
+    const newComment = [
+      areaComentario.value,
+      categoriaComentario.value,
+      selectMember.value,
+    ];
 
-      listComments.push(newComment);
+    listComments.push(newComment);
 
-      console.log(listComments);
+    console.log(listComments);
 
-      //Adicionar na tabela
-      comentario_cell.innerText = areaComentario.value;
-      categ_cell.innerText = categoriaComentario.value;
-      member_cell.innerText = selectMember.value;
-      btn_edit.innerHTML = "&#9998";
-      btn_edit.classList.add('edit_comment');
-      btn_edit.type = ("button");
+    //Adicionar na tabela
+    comentario_cell.innerText = areaComentario.value;
+    categ_cell.innerText = categoriaComentario.value;
+    member_cell.innerText = selectMember.value;
+    btn_edit.innerHTML = "&#9998";
+    btn_edit.classList.add("edit_comment");
+    btn_edit.type = "button";
 
-      //Evento para guardar edição
-      btn_edit.addEventListener("click", function () {
+    //Evento para guardar edição
+    btn_edit.addEventListener("click", function () {
+      //Botão de edição e Modal ficam visíveis
+      bEditComment.style.display = "block";
+      modal.style.display = "block";
 
-         //Botão de edição e Modal ficam visíveis
-         bEditComment.style.display = "block";
-         modal.style.display = "block";
+      document.getElementById("areaComentario").value =
+        comentario_cell.innerText;
+      document.getElementById("categoriaComentario").value =
+        categ_cell.innerText;
+      document.getElementById("selectMember").value = member_cell.innerText;
 
-         document.getElementById('areaComentario').value = comentario_cell.innerText;
-         document.getElementById('categoriaComentario').value = categ_cell.innerText;
-         document.getElementById('selectMember').value = member_cell.innerText;
+      document.getElementById("saveEdit").onclick = function () {
+        const updatedComment = [
+          document.getElementById("areaComentario").value,
+          document.getElementById("categoriaComentario").value,
+          document.getElementById("selectMember").value,
+        ];
 
-         document.getElementById("saveEdit").onclick = function() {
+        let index = listComments.indexOf(newComment);
+        if (index !== -1) {
+          listComments[index] = updatedComment;
+        }
 
-            const updatedComment = [
-               document.getElementById('areaComentario').value,
-               document.getElementById('categoriaComentario').value,
-               document.getElementById('selectMember').value
-            ];
+        // Atualiza o conteúdo da própria linha
+        comentario_cell.innerText = updatedComment[0];
+        categ_cell.innerText = updatedComment[1];
+        member_cell.innerText = updatedComment[2];
 
-            let index = listComments.indexOf(newComment);
-            if(index !== -1){
-               listComments[index]=updatedComment;
-            }
+        // Esconde o Modal
+        modal.style.display = "none";
 
-            // Atualiza o conteúdo da própria linha
-            comentario_cell.innerText = updatedComment[0];
-            categ_cell.innerText = updatedComment[1];
-            member_cell.innerText = updatedComment[2];
-    
-            // Esconde o Modal
-            modal.style.display = "none";
+        //Reset display dos botões para ficarem invisiveis
+        bGuardarComent.style.display = "none";
+        bEditComment.style.display = "none";
+      };
+    });
 
-            //Reset display dos botões para ficarem invisiveis
-            bGuardarComent.style.display = "none";
-            bEditComment.style.display = "none";
-         };
-      });
+    //AppendChilds
+    btn_edit_cell.appendChild(btn_edit);
 
-      //AppendChilds
-      btn_edit_cell.appendChild(btn_edit);
+    novaLinhaTab.appendChild(comentario_cell);
+    novaLinhaTab.appendChild(categ_cell);
+    novaLinhaTab.appendChild(member_cell);
+    novaLinhaTab.appendChild(btn_edit_cell);
 
-      novaLinhaTab.appendChild(comentario_cell);
-      novaLinhaTab.appendChild(categ_cell);
-      novaLinhaTab.appendChild(member_cell);
-      novaLinhaTab.appendChild(btn_edit_cell);
+    document.getElementById("tabelaComentarios").appendChild(novaLinhaTab);
 
-      document.getElementById("tabelaComentarios").appendChild(novaLinhaTab);
-
-      //Reset display dos botões para ficarem invisiveis
-      bGuardarComent.style.display = "none";
-      bEditComment.style.display = "none";
-
-   } else alert("All the fields are required");
+    //Reset display dos botões para ficarem invisiveis
+    bGuardarComent.style.display = "none";
+    bEditComment.style.display = "none";
+  } else alert("All the fields are required");
 };
 
 //Adicionar Membros
@@ -134,47 +141,47 @@ const members = [];
 const form = document.getElementById("formId");
 
 btn_addMember.addEventListener("click", function () {
-   if (memberInput.value != "") {
-      //Adiciona o membro à array
-      members.push(letraCaps(memberInput.value));
+  if (memberInput.value != "") {
+    //Adiciona o membro à array
+    members.push(letraCaps(memberInput.value));
 
-      console.log(members);
+    console.log(members);
 
-      //Mostrar Membros
-      let listMembers = document.getElementById("listMembers");
-      let selectMembers = document.getElementById("selectMember");
+    //Mostrar Membros
+    let listMembers = document.getElementById("listMembers");
+    let selectMembers = document.getElementById("selectMember");
 
-      //Adiciona sempre o último
-      //Lista
-      let li = document.createElement("li");
-      let close = document.createElement("span");
-      li.setAttribute("id", letraCaps(memberInput.value));
-      li.innerText = members[members.length - 1];
-      //icon close
-      close.classList.add("closeIcon");
-      close.innerText = " " + "\u00D7";
+    //Adiciona sempre o último
+    //Lista
+    let li = document.createElement("li");
+    let close = document.createElement("span");
+    li.setAttribute("id", letraCaps(memberInput.value));
+    li.innerText = members[members.length - 1];
+    //icon close
+    close.classList.add("closeIcon");
+    close.innerText = " " + "\u00D7";
 
-      //Input Select no Modal
-      let opt = document.createElement("option");
-      opt.innerText = members[members.length - 1];
-      opt.value = opt.innerText;
-      selectMembers.appendChild(opt);
+    //Input Select no Modal
+    let opt = document.createElement("option");
+    opt.innerText = members[members.length - 1];
+    opt.value = opt.innerText;
+    selectMembers.appendChild(opt);
 
-      //Evento para poder eliminar
-      close.addEventListener("click", function () {
-         const line = li.textContent.slice(0, li.textContent.length - 2);
-         removeMember(members, line);
-         li.remove();
-         opt.remove();
-      });
+    //Evento para poder eliminar
+    close.addEventListener("click", function () {
+      const line = li.textContent.slice(0, li.textContent.length - 2);
+      removeMember(members, line);
+      li.remove();
+      opt.remove();
+    });
 
-      li.appendChild(close);
-      listMembers.appendChild(li);
+    li.appendChild(close);
+    listMembers.appendChild(li);
 
-      memberInput.value = "";
-   } else {
-      alert("Write a name");
-   }
+    memberInput.value = "";
+  } else {
+    alert("Write a name");
+  }
 });
 
 //Objeto Sprint
@@ -184,46 +191,46 @@ const btn_saveSprint = document.getElementById("submitSprint");
 const list_retro = JSON.parse(localStorage.getItem("retros"));
 
 btn_saveSprint.addEventListener("click", function () {
-   if (title_sprint.value == "") {
-      if (listComments.length > 0) {
-         let atual_date = new Date();
-         atual_date = JSON.stringify(atual_date);
-         const retro = {
-            date: atual_date.slice(1, 11),
-            members: members,
-            comments: listComments,
-            title: title_sprint.value,
-         };
+  if (title_sprint.value !== "") {
+    if (listComments.length > 0) {
+      let atual_date = new Date();
+      atual_date = JSON.stringify(atual_date);
+      const retro = {
+        date: atual_date.slice(1, 11),
+        members: members,
+        comments: listComments,
+        title: title_sprint.value,
+      };
 
-         list_retro.push(retro);
+      list_retro.push(retro);
 
-         localStorage.setItem("retros", JSON.stringify(list_retro));
+      localStorage.setItem("retros", JSON.stringify(list_retro));
 
-         window.location.href = "retrospective.html";
+      window.location.href = "retrospective.html";
 
-         title_sprint.innerText = "";
-      } else alert("The Retrospective need to have a title.");
-   } else alert("Retrospective need to have comments.");
+      title_sprint.innerText = "";
+    } else alert("Retrospective need to have comments.");
+  } else alert("The Retrospective need to have a title.");
 });
 
 form.addEventListener("submit", function (e) {
-   e.preventDefault;
+  e.preventDefault;
 });
 
 //Colocar primeira letra em Maiúscula
 function letraCaps(word) {
-   const firstLetterCap = word.charAt(0).toUpperCase();
+  const firstLetterCap = word.charAt(0).toUpperCase();
 
-   const remainingLetters = word.substring(1);
+  const remainingLetters = word.substring(1);
 
-   const capWord = firstLetterCap + remainingLetters;
+  const capWord = firstLetterCap + remainingLetters;
 
-   return capWord;
+  return capWord;
 }
 
 function removeMember(list, member) {
-   let index = list.indexOf(member);
-   if (index > -1) {
-      list.splice(index, 1);
-   }
+  let index = list.indexOf(member);
+  if (index > -1) {
+    list.splice(index, 1);
+  }
 }
